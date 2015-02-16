c7258994638236ea185cbe7ed6882f09e7262357
Kevin Thornbloom <kthornbloom@gmail.com>
2015-02-15 22:13:39 -0500

    Local Storage Connection functions
    Connection functions

--- js/gui.js
+++ js/gui.js
@@ -1,3 +1,45 @@
+
+    function setConnection() {
+       var name = $('#con-name').val();
+       var data = $('#con-url').val();
+
+       localStorage.setItem(name, data);
+       showConnection();
+     }
+
+     function getConnection() {
+       var name = document.forms.editor.name.value;
+       document.forms.editor.data.value = localStorage.getItem(name);
+       showConnection();
+     }
+
+     $(document.body).on('click', '.remove-list a', function (event) {
+     	event.preventDefault();
+       var name = $(this).html();
+       localStorage.removeItem(name);
+       showConnection();
+       
+     });
+
+     function clearAll() {
+       localStorage.clear();
+       showConnection();
+     }
+
+     function showConnection() {
+       var key = "";
+       var pairs = "";
+       var i=0;
+       for (i=0; i<=localStorage.length-1; i++) {
+         key = localStorage.key(i);
+         pairs += "<a href='"+localStorage.getItem(key)+"'>"+key+"</a>";
+       }
+       $('.connection-list').html(pairs);
+       $('.remove-list').html(pairs);
+     }
+
+     showConnection();
+
 $(document).ready(function() {
 
 	// Connection Tabs 
@@ -10,6 +52,8 @@ $(document).ready(function() {
 		$('.connection-tabs .tab-content:eq('+getIndex+')').show();
 		event.preventDefault();
 	});
+
+
 	// Toggle Panels In Left Sidebar
 	$(document.body).on('click', '.panel-toggle', function (event) {
 		$(this).parents('.panel').find('.panel-contents').slideToggle(600, 'easeOutExpo');
